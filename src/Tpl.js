412846882efb04e8d8664ea64fe5d6c09fe3c371
Joaquín Fernández <jf@joaquinfernandez.net>
2018-03-19 19:08:59 +0100

    [tpl] Agregada la resolución de plantillas.

--- src/Tpl.js
+++ src/Tpl.js
@@ -1,11 +1,5 @@
-const handlebars  = require('handlebars');
-const path        = require('path');
-/**
- * Indica si los helpers por defecto se deben cargar.
- *
- * @type {Boolean}
- */
-let loadhelpers = false;
+const handlebars = require('handlebars');
+const path       = require('path');
 /**
  * Clase para manipular las plantillas usadas por los comandos.
  *
@@ -28,6 +22,23 @@ module.exports = class Tpl
          * @type     {jf.cli.Cli}
          */
         this.cli = cli;
+        /**
+         * Directorios a usar para resolver los helpers y las dependencias.
+         *
+         * @property directories
+         * @type     {Object}
+         */
+        this.directories = {};
+        //------------------------------------------------------------------------------
+        const _directories    = cli.directories;
+        const _rootDir        = cli.rootDir;
+        const _tplDirectories = this.directories;
+        Object.keys(_directories).forEach(
+            module => _tplDirectories[module] = path.resolve(_rootDir, _directories[module])
+        );
+        _tplDirectories.root = _rootDir;
+        _tplDirectories.cli  = path.resolve(__dirname, '..');
+        Object.values(_tplDirectories).forEach(dir => this.loadHelpers(path.join(dir, 'src', 'helpers')));
     }
 
     /**
@@ -75,6 +86,14 @@ module.exports = class Tpl
     generate(filename, context)
     {
         let _content;
+        if (filename[0] !== '/')
+        {
+            // Se transforma el nombre relativo a absoluto resolviendo el primer segmento
+            // que corresponde con el subdirectorio de plantillas.
+            const _filename  = filename.split('/');
+            context.relative = this.resolve(_filename.shift());
+            filename         = path.join(context.relative, ..._filename);
+        }
         if (path.extname(filename) === '.hbs')
         {
             _content = this.render(filename, context);
@@ -84,7 +103,7 @@ module.exports = class Tpl
         {
             _content = this.cli.read(filename, null);
         }
-        filename = typeof context.transform === 'function'
+        filename      = typeof context.transform === 'function'
             ? context.transform(filename)
             : filename.replace(context.relative, '');
         const _outdir = context.outdir;
@@ -92,7 +111,6 @@ module.exports = class Tpl
         {
             this.cli.write(path.join(_outdir, filename), _content, null);
         }
-
         return _content;
     }
 
@@ -103,14 +121,10 @@ module.exports = class Tpl
      */
     loadHelpers(dir)
     {
-        if (loadhelpers)
+        const _cli = this.cli;
+        if (dir && _cli.exists(dir))
         {
-            this.loadHelpers(path.join(__dirname, 'helpers'));
-            loadhelpers = false;
-        }
-        if (dir && this.exists(dir))
-        {
-            this.cli.scandir(dir).forEach(
+            _cli.scandir(dir).forEach(
                 helper => handlebars.registerHelper(path.basename(helper, '.js'), require(helper))
             );
         }
@@ -127,5 +141,28 @@ module.exports = class Tpl
     render(filename, context)
     {
         return this.compile(filename)(context).trim() + '\n';
+    }
+
+    /**
+     * Resuelve la ruta del subdirectorio de plantillas.
+     *
+     * @param {String} template Subdirectorio a resolver.
+     *
+     * @return {String|undefined} Ruta de la plantilla o `undefined` si la plantilla no se encontró.
+     */
+    resolve(template)
+    {
+        let _tpldir;
+        const _cli = this.cli;
+        for (let _directory of Object.values(this.directories))
+        {
+            _directory = path.join(_directory, 'templates', template);
+            if (_cli.exists(_directory))
+            {
+                _tpldir = _directory;
+                break;
+            }
+        }
+        return _tpldir;
     }
 };
